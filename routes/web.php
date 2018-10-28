69f674d2d771fffb54caf08f0a7cf0bfb20f38bc
Ray-PC\Ray <quzz96er43@mail.com>
2018-10-28 20:44:02 +0800

    練習4-1, 2: 為route命名

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ Route::get('welcome', function () {
     return view('welcome');
 });
 
-Route::get('hello/{name}', function ($name) {
+Route::get('hello/{name?}', ['as'=>'hello.index', function ($name='Everybody') {
     return 'Hello, '.$name;
-});
+}]);
+
 
